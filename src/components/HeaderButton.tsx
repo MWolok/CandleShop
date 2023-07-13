a7e5f97b0d29186bee675a5514df9d071ea263af
Mateusz <wmateusz993@gmail.com>
2023-07-13 18:20:22 +0200

    Add cart button comp with icon

---
+++ src/components/HeaderButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CartIcon from "./Cart/CartIcon";
+import '../components/HeaderButton.css'
+
+export default function HeaderButton() {
+	return (
+		<button className="button">
+			<span className="icon"><CartIcon/></span>
+			<span>Your Cart</span>
+            {/* Cart items num */}
+			<span className="badge">1</span>
+		</button>
+	);
+}

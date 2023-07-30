8834995edb1cd6e99dfc3bb22cfb7563e33ff5e0
Mateusz <wmateusz993@gmail.com>
2023-07-30 20:48:28 +0200

    Cart logic

--- src/components/Candles/AvailableCandles.tsx
+++ src/components/Candles/AvailableCandles.tsx
@@ -8,7 +8,7 @@ import CandleItem from './CandleItem/CandleItem';
  const candles: Candle[] = [
     {
       id: 1,
-      amount:0,
+      amount:1,
       color: "Red",
       size: "Medium",
       smell: "Rose",
@@ -16,7 +16,7 @@ import CandleItem from './CandleItem/CandleItem';
     },
     {
       id: 2,
-      amount:0,
+      amount:1,
       color: "Blue",
       size: "Large",
       smell: "Ocean Breeze",
@@ -24,7 +24,7 @@ import CandleItem from './CandleItem/CandleItem';
     },
     {
       id: 3,
-      amount:0,
+      amount:1,
       color: "Purple",
       size: "Small",
       smell: "Lavender",
@@ -32,7 +32,7 @@ import CandleItem from './CandleItem/CandleItem';
     },
     {
       id: 4,
-      amount:0,
+      amount:1,
       color: "Brown",
       size: "Medium",
       smell: "Cinnamon",
@@ -40,7 +40,7 @@ import CandleItem from './CandleItem/CandleItem';
     },
     {
       id: 5,
-      amount:0,
+      amount:1,
       color: "Ivory",
       size: "Tealight",
       smell: "Vanilla",
@@ -48,7 +48,7 @@ import CandleItem from './CandleItem/CandleItem';
     },
     {
       id: 6,
-      amount:0,
+      amount:1,
       color: "Beige",
       size: "Large",
       smell: "Sandalwood and Amber",
@@ -56,7 +56,7 @@ import CandleItem from './CandleItem/CandleItem';
     },
     {
       id: 7,
-      amount:0,
+      amount:1,
       color: "Green",
       size: "Small",
       smell: "Eucalyptus Mint",
@@ -64,7 +64,7 @@ import CandleItem from './CandleItem/CandleItem';
     },
     {
       id: 8,
-      amount:0,
+      amount:1,
       color: "White",
       size: "Taper",
       smell: "Fresh Linen",
@@ -72,7 +72,7 @@ import CandleItem from './CandleItem/CandleItem';
     },
     {
       id: 9,
-      amount:0,
+      amount:1,
       color: "Yellow",
       size: "Medium",
       smell: "Citrus Burst",
@@ -80,7 +80,7 @@ import CandleItem from './CandleItem/CandleItem';
     },
     {
       id: 10,
-      amount:0,
+      amount:1,
       color: "Dark Green",
       size: "Medium",
       smell: "Patchouli and Cedarwood",
--- src/components/store/CartContext.tsx
+++ src/components/store/CartContext.tsx
@@ -1,7 +1,6 @@
 import { createContext, useState } from "react";
 import { Candle } from "../Candles/Candle";
 
-
 export type CartValue = {
 	items: Candle[];
 	totalAmount: number;
@@ -20,27 +19,49 @@ export const CartProvider = (props: React.PropsWithChildren) => {
 	const [amount, setAmount] = useState(0);
 
 	const addItem = (item: Candle) => {
+		
+		const idOfExistingItem = items.findIndex((it) => it.id === item.id);
+		
+		if (idOfExistingItem !== -1) {
+			console.log("true")
+		
+		
+			const updatedItems = items.map((it, index) =>
+			index === idOfExistingItem ? { ...it, amount: it.amount + 1 } : it
+		  );
+		  setItems(updatedItems);
+			// const updateObject = { ...items[idOfExistingItem], amount: amount + 1 };
+			// const updateState = [
+			// 	...items.slice(9, idOfExistingItem),
+			// 	updateObject,
+			// 	...items.slice(idOfExistingItem + 1),
+			// ];
+			// setItems(updateState);
+			// return;
+		}else {
+			setItems([...items, item]);
+		}
 
-		setItems([...items, item]);
-		setAmount((prevAmount) => prevAmount+1)
+		
+		setAmount((prevAmount) => prevAmount + 1);
 	};
+
 	const removeItem = (id: number) => {
-		if(amount>1){
-		const newArray = items.filter((item) => item.id !== id);
-		setItems(newArray);
-		setAmount((prevAmount) => prevAmount-1)}
+		if (amount > 1) {
+			const newArray = items.filter((item) => item.id !== id);
+			setItems(newArray);
+			setAmount((prevAmount) => prevAmount - 1);
+		}
 	};
-    return (
-        <CartContext.Provider
-          value={{
-            items: items,
-            totalAmount: amount,
-            addItem: addItem,
-            removeItem: removeItem,
-          }}
-        >
-          {props.children}
-        </CartContext.Provider>
-      );
-
+	return (
+		<CartContext.Provider
+			value={{
+				items: items,
+				totalAmount: amount,
+				addItem: addItem,
+				removeItem: removeItem,
+			}}>
+			{props.children}
+		</CartContext.Provider>
+	);
 };

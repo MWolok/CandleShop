28cf211c4aa97539849acea7e62e5c05fbb98838
Mateusz <wmateusz993@gmail.com>
2023-08-01 17:26:32 +0200

    add  bump animation

--- src/components/HeaderButton.tsx
+++ src/components/HeaderButton.tsx
@@ -1,13 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import CartIcon from "./Cart/CartIcon";
 import "../components/HeaderButton.css";
 import { CartContext } from "./store/CartContext";
 
 export default function HeaderButton(props: any) {
+	const [btnAnimation, setBtnAnimation] = useState(false);
 	const cart = useContext(CartContext);
-	console.log(cart.totalAmount);
+	const { items } = cart;
+
+	const btnClass = `${"button"} ${btnAnimation ? "bump" : ""}`;
+
+	useEffect(() => {
+		if (cart.items.length === 0) {
+			return;
+		}
+		setBtnAnimation(true);
+		const time = setTimeout(() => {
+			setBtnAnimation(false);
+		}, 300);
+
+		return () => {
+			clearTimeout(time);
+		};
+	}, [items]);
+
 	return (
-		<button className="button" onClick={props.onClick}>
+		<button className={btnClass} onClick={props.onClick}>
 			<span className="icon">
 				<CartIcon />
 			</span>
